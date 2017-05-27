691927e9b85fe646d2c363efd5bc7f94ae36f7de
Mike Lin <linmxy@gmail.com>
2017-05-28 00:02:38 +0800

    palindrome number answer

--- palindrome_number/answer.js
+++ palindrome_number/answer.js
@@ -1,3 +1,12 @@
-module.exports = function (number) {
-  return true;
+module.exports = function (x) {
+  if (x < 0 || (x != 0 && x % 10 == 0)) {
+    return false;
+  }
+  //only reversing till half and then compare
+  var sum =  0;
+  while (x > sum) {
+    sum = sum * 10 + x % 10;
+    x = x / 10;
+  }
+  return (x == sum) || (x == sum / 10);
 };
